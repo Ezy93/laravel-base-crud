37f8a242bb76c533f2a524c29568a60491ff51ad
Ezy93 <giordani001@gmail.com>
2022-05-17 16:42:13 +0200

    init main layouts, partials, comics index, comics show and change route use resource

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('title', 'comic')
+
+@section('main-content')
+
+@endsection
+
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{route('comics.index')}}">Comics</a>
+        </div>
+    </nav>
+</header>
