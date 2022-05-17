bc4d01007de4654e65bc8ce9bab908d9df5b3aa8
Ezy93 <giordani001@gmail.com>
2022-05-17 17:25:04 +0200

    add create blade, use Route::resource instead ::get, add return to create function in controller

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -30,7 +30,7 @@ class ComicsController extends Controller
      */
     public function create()
     {
-        //
+        return view('comics.create');
     }
 
     /**
---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'add comic')
+
+@section('main-content')
+
+@endsection
